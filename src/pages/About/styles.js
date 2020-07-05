1a99180d9407f7d9268d378ba440ef4ea31452eb
irailtonreis <irailton.oliveira91@gmail.com>
2020-07-05 20:33:10 -0300

    style: adiciona css pagina sobre

---
+++ src/pages/About/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 100px;
+  width: 100%;
+  max-width: 1120px;
+  height: 80vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
